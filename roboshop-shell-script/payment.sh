efbd7a184e8406c273fad7360bcb453ce0e5696b
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2023-12-17 00:11:34 +0530

    script mistakes

--- roboshop-shell-script/payment.sh
+++ roboshop-shell-script/payment.sh
@@ -10,12 +10,13 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NC="\e[0m"
 
-echo "Script started executing at $TIMESTAMP" &>>LOGFILE
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
 
 #Check for root user
 if [ $ID -ne 0 ]
 then
   echo -e "$RED ERROR: Please run script with root access$NC"
+  exit
 else
   echo -e "$GREEN You are root user$NC"
 fi
@@ -31,11 +32,11 @@ VALIDATE() {
 }
 
 echo -e "$YELLOW Installing python.....$NC"
-dnf install python36 gcc python3-devel -y &>>LOGFILE
+dnf install python36 gcc python3-devel -y &>>$LOGFILE
 VALIDATE $? "Installing python"
 
 #creating user
-id roboshop  &>>LOGFILE
+id roboshop  &>>$LOGFILE
 if [ $? -ne 0 ]
 then
   useradd roboshop 
@@ -44,30 +45,30 @@ else
   echo -e "$YELLOW roboshop user already exist...skipping$NC"
 fi
 
-mkdir -p /app  &>>LOGFILE
+mkdir -p /app  &>>$LOGFILE
 VALIDATE $? "Creating directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>>LOGFILE
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>>$LOGFILE
 VALIDATE $? "Downloading payment src"
 
-cd /app &>>LOGFILE
+cd /app &>>$LOGFILE
 VALIDATE $? "Change directory"
 
-unzip -o /tmp/payment.zip &>>LOGFILE
+unzip -o /tmp/payment.zip &>>$LOGFILE
 VALIDATE $? "Unzip src"
 
-pip3.6 install -r requirements.txt &>>LOGFILE
+pip3.6 install -r requirements.txt &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
-cp /home/centos/roboshop-shell-script/payment.service /etc/systemd/system/payment.service &>>LOGFILE
+cp /home/centos/roboshop-shell-script/payment.service /etc/systemd/system/payment.service &>>$LOGFILE
 VALIDATE $? "Copying payment.service file"
 
-systemctl daemon-reload &>>LOGFILE
+systemctl daemon-reload &>>$LOGFILE
 VALIDATE $? "Daemon reload"
 
-systemctl enable payment  &>>LOGFILE
+systemctl enable payment  &>>$LOGFILE
 VALIDATE $? "Enable payment service"
 
-systemctl start payment &>>LOGFILE
+systemctl start payment &>>$LOGFILE
 VALIDATE $? "Start payment service"
 
