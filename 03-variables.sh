f554262b9924551515e2bb0c2da3c1607e6d77bd
NarayanadasuRakesh <126420803+NarayanadasuRakesh@users.noreply.github.com>
2023-12-16 22:24:05 +0530

    Delete 03-variables.sh

--- 03-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# using variables
-PERSON1=Ramesh
-PERSON2=Suresh
-echo "$PERSON1: Good morning $PERSON2"
-echo "$PERSON2: very good morining $PERSON1"
-echo "$PERSON1: how are you?"
-echo "$PERSON2: I am good, how are you doing?"
-echo "$PERSON1: I am doing great"
