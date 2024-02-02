e8581dad3ecf6123f91657d57a384b6f1cbb1ed6
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-02-03 00:18:23 +0530

    fix: main.tf

--- terraform/data-source/main.tf
+++ terraform/data-source/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-    ami = "aws_ami.aws-linux-2.id" # retrieve id using data.tf filter
+    ami = "aws_ami.centos8.id" # retrieve id using data.tf filter
     instance_type = var.instance_type
     tags = var.tags
 }
